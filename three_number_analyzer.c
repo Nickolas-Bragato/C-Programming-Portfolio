630f12a6c4b80e1978d00fb48075f9654ec18c2f
Nickolas Sponton Pires Bragato <nickolas.bragato@gmail.com>
2025-11-17 17:46:20 -0300

    Update and rename Tarefa-2.19.c to three_number_analyzer.c

--- three_number_analyzer.c
+++ three_number_analyzer.c
@@ -5,40 +5,40 @@
 void main(){
   setlocale(LC_ALL, "Portuguese");
 
-   /* Exercício 2.19*/
+   /* ExercÃ­cio 2.19*/
 
   int num1, num2, num3, soma, produto;
   float media;
 
-  printf("Entre com três inteiros diferentes:");
+  printf("Entre com trÃªs inteiros diferentes:");
   scanf("%d %d %d",&num1,&num2,&num3);
 
   soma=num1+num2+num3;
   media=soma/3;
   produto=num1*num2*num3;
 
-  printf("A soma é %d \n", soma);
-  printf("A média é %.2f \n", media);
-  printf("O produto é %d \n", produto);
+  printf("A soma Ã© %d \n", soma);
+  printf("A mÃ©dia Ã© %.2f \n", media);
+  printf("O produto Ã© %d \n", produto);
 
   if (num1>num2 && num1>num3)
-  	printf("O maior é %d \n", num1);
+  	printf("O maior Ã© %d \n", num1);
 
   if (num2>num1 && num2>num3)
-  	printf("O maior é %d \n", num2);
+  	printf("O maior Ã© %d \n", num2);
 
   if (num3>num2 && num3>num1)
-  	printf("O maior é %d \n", num3);
+  	printf("O maior Ã© %d \n", num3);
 
   	/* menor */
 
   if (num1<num2 && num1<num3)
-  	printf("O menor é %d", num1);
+  	printf("O menor Ã© %d", num1);
 
   if (num2<num1 && num2<num3)
-  	printf("O menor é %d", num2);
+  	printf("O menor Ã© %d", num2);
 
   if (num3<num2 && num3<num1)
-  	printf("O menor é %d", num3);
+  	printf("O menor Ã© %d", num3);
   }
 
